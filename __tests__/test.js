632b7812be7eed44947d4d3d01b311b106e22109
Vetrash <Vetrash47@gmail.com>
2022-07-25 10:54:07 +0500

    Исправление вторых замечаний
    Переписал тесты в компактную форму
    в index.js сделал выводы результатов функций без сохранения в переменную

--- __tests__/test.js
+++ __tests__/test.js
@@ -3,33 +3,19 @@ import fs from 'fs';
 import path from 'path';
 import getDiff from '../src/index.js';
 
-test('Checking the Structure of a Response getDiff flat object -stylish', () => {
-  const normPathFlat = path.resolve('__fixtures__', 'expectedFlat.txt');
-  const expectedFlat = fs.readFileSync(normPathFlat, 'utf-8');
+const getExpected = (nameFile) => {
+  const normPathFlat = path.resolve('__fixtures__', nameFile);
+  return fs.readFileSync(normPathFlat, 'utf-8');
+};
 
-  expect(expectedFlat).toEqual(getDiff('__fixtures__/fileFlat1.json', '__fixtures__/fileFlat2.json'));
-  expect(expectedFlat).toEqual(getDiff('__fixtures__/fileFlat1.yml', '__fixtures__/fileFlat2.yml'));
-});
-
-test('Checking the Structure of a Response getDiff deep object -stylish', () => {
-  const normPathDeep = path.resolve('__fixtures__', 'expectedDeep.txt');
-  const expectedDeep = fs.readFileSync(normPathDeep, 'utf-8');
-
-  expect(expectedDeep).toEqual(getDiff('__fixtures__/fileDeep1.json', '__fixtures__/fileDeep2.json'));
-  expect(expectedDeep).toEqual(getDiff('__fixtures__/fileDeep1.yml', '__fixtures__/fileDeep2.yml'));
-});
-
-test('Checking the Structure of a Response getDiff deep object -plain', () => {
-  const normPathDeep = path.resolve('__fixtures__', 'expectedDeep-Plain.txt');
-  const expectedDeep = fs.readFileSync(normPathDeep, 'utf-8');
-
-  expect(expectedDeep).toEqual(getDiff('__fixtures__/fileDeep1.json', '__fixtures__/fileDeep2.json', 'plain'));
-  expect(expectedDeep).toEqual(getDiff('__fixtures__/fileDeep1.yml', '__fixtures__/fileDeep2.yml', 'plain'));
-});
-
-test('Checking the Structure of a Response getDiff deep object -json', () => {
-  const normPathDeep = path.resolve('__fixtures__', 'expectedDeep-Json.txt');
-  const expectedDeep = fs.readFileSync(normPathDeep, 'utf-8');
-  expect(expectedDeep).toEqual(getDiff('__fixtures__/fileDeep1.json', '__fixtures__/fileDeep2.json', 'json'));
-  expect(expectedDeep).toEqual(getDiff('__fixtures__/fileDeep1.yml', '__fixtures__/fileDeep2.yml', 'json'));
+test.each([
+  ['json', 'stylish', 'expectedDeep.txt'],
+  ['json', 'plain', 'expectedDeep-Plain.txt'],
+  ['json', 'json', 'expectedDeep-Json.txt'],
+  ['yml', 'stylish', 'expectedDeep.txt'],
+  ['yml', 'plain', 'expectedDeep-Plain.txt'],
+  ['yml', 'json', 'expectedDeep-Json.txt'],
+])('.add(format file: %s, formatters: %s)', (formatFile, formatters, expFileName) => {
+  const expected = getExpected(expFileName);
+  expect(expected).toEqual(getDiff(`__fixtures__/fileDeep1.${formatFile}`, `__fixtures__/fileDeep2.${formatFile}`, formatters));
 });
--- src/index.js
+++ src/index.js
@@ -8,15 +8,13 @@ const getData = (filePath) => {
   const normalizePath = path.resolve(process.cwd(), filePath);
   const dataFile = fs.readFileSync(normalizePath, 'utf-8');
   const format = path.extname(normalizePath).slice(1);
-  const parseData = parser(dataFile, format);
-  return parseData;
+  return parser(dataFile, format);
 };
 
 const getDiff = (filePath1, filePath2, format = 'stylish') => {
   const dataFile1 = getData(filePath1);
   const dataFile2 = getData(filePath2);
   const differenceTree = buildDiffTree(dataFile1, dataFile2);
-  const solution = formaters(differenceTree, format);
-  return solution;
+  return formaters(differenceTree, format);
 };
 export default getDiff;
